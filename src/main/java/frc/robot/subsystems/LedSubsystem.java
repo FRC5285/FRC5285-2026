87970fcf124dea5f1842f723ce9d67d63a065f14
eatean <ethanyangtaco115@gmail.com>
2026-02-02 18:42:03 -0800

    fix everything

--- src/main/java/frc/robot/subsystems/LedSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,14 +14,34 @@ import frc.robot.Constants.LEDConstants;
 
 
 public class LedSubsystem extends SubsystemBase {
-    // Instance variables go here
+    private final Spark LED = new Spark(LEDConstants.led_pin);
+    public long[] ledPatternState = null;
+    public double[] currentPattern = null;
+    private double currentValue = 0.0;
     
-    public LedSubsystem() {
 
 
-        SendableRegistry.add(this, "LEDs");
-        SmartDashboard.putData(this);
+    public void ledPattern(double[] arr) {
+    if (arr == null || arr.length < 2) return;
+
+    long now = System.currentTimeMillis();
+
+    if (ledPatternState == null) {
+        ledPatternState = new long[]{now, 1, (long)(1000 / arr[0])};
+        currentPattern = arr;
+        currentValue = arr[1];
+        return;
     }
+
+    if (now - ledPatternState[0] >= ledPatternState[2]) {
+        int index = (int) ledPatternState[1] + 1;
+        if (index >= arr.length) index = 1;
+
+        currentValue = arr[index];
+        ledPatternState[0] = now;
+        ledPatternState[1] = index;
+    }
+}
 
     // Other methods go here
     public void setPattern(double[] pattern) {
