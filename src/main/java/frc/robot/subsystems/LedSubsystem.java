ee10dcbae831923b8a3f50c25bc2e5d60f01155c
eatean <ethanyangtaco115@gmail.com>
2026-02-02 18:33:11 -0800

    all done

--- src/main/java/frc/robot/subsystems/LedSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -4,8 +4,8 @@ import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.Constants.LEDConstants;
 
 //global usage, only one pattern at a time, 
@@ -49,11 +49,21 @@ public class LedSubsystem extends SubsystemBase {
 
 
     // Other methods go here
-
+    public void setPattern(double[] pattern) {
+        ledPatternState = null;
+        currentPattern = pattern;
+    }
+    
+    public Command stopMotor() {
+        return runOnce(() -> LED.stopMotor());
+    }
 
     @Override
     public void periodic() {
-
+        if (currentPattern != null) {
+            ledPattern(currentPattern);
+            LED.set(currentValue);
+    }
     }
 
     @Override
