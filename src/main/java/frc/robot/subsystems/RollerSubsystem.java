16d55cf9c2132130ca4d559d71ecb2b738c146ff
Saam Mazaheri <mazaheri.saam@gmail.com>
2026-03-01 12:54:33 -0800

    good

--- src/main/java/frc/robot/subsystems/RollerSubsystem.java
+++ src/main/java/frc/robot/subsystems/RollerSubsystem.java
@@ -1,30 +1,39 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.util.sendable.SendableBuilder;
-import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class RollerSubsystem extends SubsystemBase {
-    // Instance variables go here
+
     
+    private final TalonFX rollerMotor;
+    private final DutyCycleOut dutyCycle = new DutyCycleOut(0);
+
+    private static final int ROLLER_MOTOR_ID = 5;
+
     public RollerSubsystem() {
+        rollerMotor = new TalonFX(ROLLER_MOTOR_ID);
 
+        // Motor configuration
+        TalonFXConfiguration config = new TalonFXConfiguration();
+        config.CurrentLimits.SupplyCurrentLimit = 40;
+        config.CurrentLimits.SupplyCurrentLimitEnable = true;
 
-        SendableRegistry.add(this, "Storage Rollers");
-        SmartDashboard.putData(this);
+        rollerMotor.getConfigurator().apply(config);
     }
 
-    // Other methods go here
 
-
-    @Override
-    public void periodic() {
-
+    public void run() {
+        rollerMotor.setControl(dutyCycle.withOutput(0.8));
     }
 
-    @Override
-    public void initSendable(SendableBuilder builder) {
-        
+    
+    public void stop() {
+        rollerMotor.setControl(dutyCycle.withOutput(0.0));
     }
 }
+
+
